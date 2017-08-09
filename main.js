39ba8d7654f32d38a1813fa321c7770491bc9696
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-08-09 13:14:15 -0500

    fix HTML form, fix search

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 let songFinder = document.querySelector("input");
 let songContainer = document.querySelector(".container");
+let formSubmitter = document.querySelector(".submit");
 const enterKeyCode = 13;
 
-songFinder.addEventListener("keydown", function(e) {
+formSubmitter.addEventListener("click", function(e) {
   let url =
     "https://itunes.apple.com/search?term=" + songFinder.value + "&entity=song";
+  e.preventDefault();
+  axios.get(url).then(function(response) {
+    songContainer.innerHTML = ``;
+    let data = response.data.results;
+    data.forEach(function(data) {
+      let searchResults = ``;
 
-  if (event.keyCode === enterKeyCode) {
-    axios.get(url).then(function(response) {
-      let data = response.resultCount.results;
-      data.forEach(function(data) {
-        let searchResults = ``;
-        searchResults = ``;
+      searchResults = `<div class ="returnItem"><a href="${data.previewUrl}"><img class="albumArt" src="${data.artworkUrl100}"></a>
+        <div class="songTitle">${data.trackName}</div>
+        <div class="bandName"><a href="${data.artistViewUrl}">${data.artistName}</a></div>
+        </div>
+        `;
 
-        songContainer.innerHTML += searchResults;
-      });
+      songContainer.innerHTML += searchResults;
     });
-  }
+  });
 });
