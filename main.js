eb86d5f21a263a059fbfdd56930340f723a96711
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-08-09 11:18:05 -0500

    fix repo issue and added audio element

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-let recipeSearch = document.querySelector("input");
-let recipeContainer = document.querySelector(".container");
+let songFinder = document.querySelector("input");
+let songContainer = document.querySelector(".container");
 const enterKeyCode = 13;
 
-recipeSearch.addEventListener("keydown", function(e) {
+songFinder.addEventListener("keydown", function(e) {
   let url =
-    "http://recipepuppyproxy.herokuapp.com/api/?q=" + recipeSearch.value;
+    "https://itunes.apple.com/search?term=" + songFinder.value + "&entity=song";
 
   if (event.keyCode === enterKeyCode) {
     axios.get(url).then(function(response) {
       let data = response.data.results;
       data.forEach(function(data) {
         let searchResults = ``;
-        searchResults = `
-            <div class="recipe" style="background-image: url(${data.thumbnail}), 
-            url(https://www.placecage.com/c/140/200)"><p class="recipeTitle"><a href="${data.href}">${data.title}</a></p>
-            </div>`;
+        searchResults = ``;
 
-        recipeContainer.innerHTML += searchResults;
+        songContainer.innerHTML += searchResults;
       });
     });
   }
